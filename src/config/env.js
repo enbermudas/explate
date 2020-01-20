7058048ecec53686e7a3719c0577cdac72848723
Enrique Bermúdez <enrique.bermudez@4rsoluciones.com>
2020-01-20 10:55:44 -0300

    style(config): lint env configuration file

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const HOST =  process.env.HOST ||  '0.0.0.0';
-const PORT =  process.env.PORT ||  '3000';
-const LOG_LEVEL =  process.env.LOG_LEVEL ||  'debug';
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || '3000';
+const LOG_LEVEL = process.env.LOG_LEVEL || 'debug';
 
 module.exports = {
   NODE_ENV,
