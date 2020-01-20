7ac890037cffe1b3592b56083c9baf45edb703a1
Enrique Bermúdez <enrique.bermudez@4rsoluciones.com>
2020-01-20 16:56:01 -0300

    fix(config): change exported consts declaration on env configuration

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
 require('dotenv-safe').config();
 
-const NODE_ENV = process.env.NODE_ENV;
-const HOST = process.env.HOST;
-const PORT = process.env.PORT;
-const LOG_LEVEL = process.env.LOG_LEVEL;
+const { NODE_ENV } = process.env;
+const { HOST } = process.env;
+const { PORT } = process.env;
+const { LOG_LEVEL } = process.env;
 
 module.exports = {
   NODE_ENV,
