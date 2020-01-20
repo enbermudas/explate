2f7fe45f413182bcc7c010261e0ed6a70bfc2dd5
Enrique Bermúdez <enrique.bermudez@4rsoluciones.com>
2020-01-20 17:00:53 -0300

    style(config): turn multiple object destructuring into a single one

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,11 @@
 require('dotenv-safe').config();
 
-const { NODE_ENV } = process.env;
-const { HOST } = process.env;
-const { PORT } = process.env;
-const { LOG_LEVEL } = process.env;
+const {
+  NODE_ENV,
+  HOST,
+  PORT,
+  LOG_LEVEL
+} = process.env;
 
 module.exports = {
   NODE_ENV,
